88cb8ba67f396d61686843d78bea69460068716d
Yasin Raies <raies@posteo.de>
2020-05-12 18:13:15 +0200

    Added smoothed statistics to viz

--- rayviz/src/main.rs
+++ rayviz/src/main.rs
@@ -1,17 +1,17 @@
-use std::env;
-use std::time::Duration;
-use std::time::SystemTime;
-//use crate::uniform::Uniform;
-//use crate::vos::DrawType;
-//use crate::vos::Usage;
 use crustswarm_lib as crustswarm;
+
+use std::fs::File;
+use std::io::Write;
+use std::ops::Add;
+use std::string::String;
+use std::time::{Duration, SystemTime};
+
+use circular_queue::CircularQueue;
+
+use tempfile::TempDir;
 
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
-
-use std::fs::File;
-use std::io::{self, Read, Write};
-use tempfile::TempDir;
 
 use raylib::prelude::*;
 
@@ -54,10 +54,8 @@ fn main() {
         temp.start_dist.clone().apply(temp, &mut rnd)
     };
 
-    let mut render_instant = SystemTime::now();
-    let mut render_delta = Duration::new(0, 0);
-    let mut sim_delta = Duration::new(0, 0);
-
+    let mut render_stats = VizStats::new();
+    let mut sim_stats = VizStats::new();
     let mut calc_next = true;
     let mut draw_buoy = true;
 
@@ -66,81 +64,90 @@ fn main() {
         .expect("Could not load font");
 
     while !rl.window_should_close() {
-        rl.update_camera(&mut camera);
-
-        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
-            calc_next = !calc_next;
-        }
-
-        if rl.is_key_pressed(KeyboardKey::KEY_B) {
-            draw_buoy = !draw_buoy;
-        }
-
-        let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::BLACK);
+        // Update World State
         {
-            let mut d3d = d.begin_mode_3D(camera);
-
-            d3d.draw_grid(10, 10.0);
-
-            render_delta = render_instant.elapsed().unwrap_or(Duration::new(0, 0));
-            let sim_instant = SystemTime::now();
             if calc_next {
+                sim_stats.start();
                 sg.step(&mut rnd);
+                sim_stats.stop();
             }
-            sim_delta = sim_instant.elapsed().unwrap_or(Duration::new(0, 0));
-            render_instant = SystemTime::now();
+        }
 
-            let agents = crustswarm::agents_to_arr2(&sg);
-            for (pos, color_index) in agents {
-                d3d.draw_cube(
-                    Vector3::new(pos[0], pos[1], pos[2]),
-                    1.0,
-                    1.0,
-                    1.0,
-                    get_color(color_index),
-                );
+        // Handle Inputs
+        {
+            if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+                calc_next = !calc_next;
             }
 
-            if draw_buoy {
-                let buoys = crustswarm::buoys_to_arr2(&sg);
-                for pos in buoys {
+            if rl.is_key_pressed(KeyboardKey::KEY_B) {
+                draw_buoy = !draw_buoy;
+            }
+
+            rl.update_camera(&mut camera);
+        }
+
+        // Draw the Scene
+        {
+            render_stats.start();
+            let mut d = rl.begin_drawing(&thread);
+            d.clear_background(Color::BLACK);
+
+            // Draw 3D Stuff
+            {
+                let mut d3d = d.begin_mode_3D(camera);
+
+                d3d.draw_grid(10, 10.0);
+
+                let agents = crustswarm::agents_to_arr2(&sg);
+                for (pos, color_index) in agents {
                     d3d.draw_cube(
                         Vector3::new(pos[0], pos[1], pos[2]),
-                        0.75,
-                        0.75,
-                        0.75,
-                        get_color(8),
+                        1.0,
+                        1.0,
+                        1.0,
+                        get_color(color_index),
                     );
                 }
+
+                if draw_buoy {
+                    let buoys = crustswarm::buoys_to_arr2(&sg);
+                    for pos in buoys {
+                        d3d.draw_cube(
+                            Vector3::new(pos[0], pos[1], pos[2]),
+                            0.75,
+                            0.75,
+                            0.75,
+                            get_color(8),
+                        );
+                    }
+                }
             }
-        }
 
-        {
-            let infostring = format!(
-                "FPS: {: >2}
-Rendertime:{:>3}ms
-Simtime:   {:>3}ms",
-                &d.get_fps(),
-                &render_delta.as_millis(),
-                &sim_delta.as_millis(),
-            );
-            draw_text(&mut d, &font, 5, 5, &infostring);
+            // Draw UI Stuff
+            {
+                let stat_info = format!(
+                    "FPS: {:2}\n{}\n{}",
+                    d.get_fps(),
+                    render_stats.to_string("Render", 6, false),
+                    sim_stats.to_string("Sim", 6, false),
+                );
+                draw_text(&mut d, &font, 5, 5, &stat_info);
 
-            //d.draw_text("", 20, 30, 10, Color::DARKGRAY);
-            d.draw_text(
-                "Free camera default controls:
+                //d.draw_text("", 20, 30, 10, Color::DARKGRAY);
+                d.draw_text(
+                    "Free camera default controls:
 - Mouse Wheel to Zoom in-out
 - Mouse Wheel Pressed to Pan
 - Alt + Mouse Wheel Pressed to Rotate
 - Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom
 - Z to zoom to (0, 0, 0)",
-                10,
-                d.get_screen_height() - 100,
-                10,
-                Color::GRAY,
-            );
+                    10,
+                    d.get_screen_height() - 100,
+                    10,
+                    Color::GRAY,
+                );
+            }
+            render_stats.stop();
         }
     }
 
@@ -167,88 +174,142 @@ Simtime:   {:>3}ms",
             Color::WHITE,
         );
     }
-    /*
-            while !window.should_close() {
-                //// Handle Input
-
-                glfw.poll_events();
-                for (_, event) in glfw::flush_messages(&events) {
-                    match event {
-                        glfw::WindowEvent::Key(Key::Space, _, Action::Press, _) => {
-                            calc_next = !calc_next;
-                        }
-                        glfw::WindowEvent::Key(Key::M, _, Action::Press, _) => {
-                            draw_buoy = !draw_buoy;
-                        }
-                        _ => {}
-                    }
-                }
-    */
-    /*
-            //// Recalc delay/time
-            println!("{:?}", last_step.elapsed());
-            thread::sleep(
-                frametime
-                    .checked_sub(last_step.elapsed().unwrap())
-                    .unwrap_or(Duration::new(0, 0)),
-            );
-            last_step = SystemTime::now();
-
-            shader.use_program();
-
-            if calc_next {
-                sg.step(&mut rnd);
-                agent_vo.update_buffer(crustswarm::agents_to_arr(&sg), Usage::Stream);
-            }
-
-            //// Set camera
-            uni_view.update(camera.get_view_matrix());
-            buoy_shader.use_program();
-            ubi_view.update(camera.get_view_matrix());
-
-            //// Render
-            unsafe {
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-                gl::Clear(gl::DEPTH_BUFFER_BIT);
-            }
-
-            shader.use_program();
-            uni_mark.update(1);
-            floor_vo.draw(DrawType::Points);
-            println!("f{}", floor_vo.get_entry_count());
-
-            uni_mark.update(0);
-            agent_vo.draw(DrawType::Points);
-            println!("a{}", agent_vo.get_entry_count());
-
-            buoy_shader.use_program();
-            if draw_buoy {
-                buoy_vo.update_buffer(crustswarm::buoys_to_arr(&sg), Usage::Stream);
-                buoy_vo.draw(DrawType::Points);
-            }
-            println!("b{}", buoy_vo.get_entry_count());
-
-            window.swap_buffers();
-        }
-
-        //    println!("{:?}", crustswarm::swarm_to_arr(&sg));
-    }
-
-    fn gen_floor_mark(spacing: f32, count: usize) -> Vec<f32> {
-        for i_ in 1..=count {
-            let i = (i_ as f32) * spacing;
-            mark.push(i);
-            mark.push(0.0);
-            mark.push(0.0);
-            mark.push(5.0);
-
-            color 5
-            Vector3::new( i,0.0,0.0)
-            Vector3::new(-i,0.0,0.0)
-            color 4
-            Vector3::new(0.0,0.0, i)
-            Vector3::new(0.0,0.0,-i)
-        }
-    }
-        )*/
 }
+
+type Prec = u32;
+const WINDOW: usize = 10;
+const BASE_INFO_WIDTH: usize = 4;
+
+pub struct VizStats {
+    pub start: SystemTime,
+    pub queue: CircularQueue<Prec>,
+}
+
+impl VizStats {
+    fn new() -> VizStats {
+        VizStats {
+            start: SystemTime::now(),
+            queue: CircularQueue::with_capacity(WINDOW),
+        }
+    }
+    fn avg_millis(&self) -> f32 {
+        let (c, total) = self.queue.iter().fold((0, 0f32), |(count, acc), next| {
+            (count + 1, acc + (*next as f32))
+        });
+        if c > 0 {
+            total / (c as f32 * 1000f32)
+        } else {
+            total / 1000f32
+        }
+    }
+    fn to_string(&self, title: &str, width: usize, verbose: bool) -> String {
+        let text = String::from(title).add(":");
+        let p = BASE_INFO_WIDTH + width.saturating_sub(title.len());
+        let avg_time = self.avg_millis();
+        let smoothed = format!("{0}{1:>2$.1}ms", text, avg_time, p);
+        if verbose {
+            let unsmoothed = format!("{0:>1$.1}ms", self.get_time(), p + text.len());
+            format!("{}\n{}", smoothed, unsmoothed)
+        } else {
+            smoothed
+        }
+    }
+    fn start(&mut self) {
+        self.start = SystemTime::now()
+    }
+    fn stop(&mut self) {
+        self.queue.push(
+            self.start
+                .elapsed()
+                .unwrap_or_else(|_| Duration::new(0, 0))
+                .as_micros() as Prec,
+        );
+    }
+    fn get_time(&self) -> f32 {
+        self.queue.iter().next().unwrap_or(&0).to_owned() as f32 / 1000f32
+    }
+}
+/*
+        while !window.should_close() {
+            //// Handle Input
+
+            glfw.poll_events();
+            for (_, event) in glfw::flush_messages(&events) {
+                match event {
+                    glfw::WindowEvent::Key(Key::Space, _, Action::Press, _) => {
+                        calc_next = !calc_next;
+                    }
+                    glfw::WindowEvent::Key(Key::M, _, Action::Press, _) => {
+                        draw_buoy = !draw_buoy;
+                    }
+                    _ => {}
+                }
+            }
+*/
+/*
+        //// Recalc delay/time
+        println!("{:?}", last_step.elapsed());
+        thread::sleep(
+            frametime
+                .checked_sub(last_step.elapsed().unwrap())
+                .unwrap_or(Duration::new(0, 0)),
+        );
+        last_step = SystemTime::now();
+
+        shader.use_program();
+
+        if calc_next {
+            sg.step(&mut rnd);
+            agent_vo.update_buffer(crustswarm::agents_to_arr(&sg), Usage::Stream);
+        }
+
+        //// Set camera
+        uni_view.update(camera.get_view_matrix());
+        buoy_shader.use_program();
+        ubi_view.update(camera.get_view_matrix());
+
+        //// Render
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::Clear(gl::DEPTH_BUFFER_BIT);
+        }
+
+        shader.use_program();
+        uni_mark.update(1);
+        floor_vo.draw(DrawType::Points);
+        println!("f{}", floor_vo.get_entry_count());
+
+        uni_mark.update(0);
+        agent_vo.draw(DrawType::Points);
+        println!("a{}", agent_vo.get_entry_count());
+
+        buoy_shader.use_program();
+        if draw_buoy {
+            buoy_vo.update_buffer(crustswarm::buoys_to_arr(&sg), Usage::Stream);
+            buoy_vo.draw(DrawType::Points);
+        }
+        println!("b{}", buoy_vo.get_entry_count());
+
+        window.swap_buffers();
+    }
+
+    //    println!("{:?}", crustswarm::swarm_to_arr(&sg));
+}
+
+fn gen_floor_mark(spacing: f32, count: usize) -> Vec<f32> {
+    for i_ in 1..=count {
+        let i = (i_ as f32) * spacing;
+        mark.push(i);
+        mark.push(0.0);
+        mark.push(0.0);
+        mark.push(5.0);
+
+        color 5
+        Vector3::new( i,0.0,0.0)
+        Vector3::new(-i,0.0,0.0)
+        color 4
+        Vector3::new(0.0,0.0, i)
+        Vector3::new(0.0,0.0,-i)
+    }
+}
+    )*/
